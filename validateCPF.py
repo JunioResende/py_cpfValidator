98bad0feebb4ddf5b812d581b802dbad02ee2c6f
JunioResende <junioresende23@gmail.com>
2021-10-19 12:26:08 -0300

    gix

--- validateCPF.py
+++ validateCPF.py
@@ -1,30 +1,30 @@
 while True:
     cpf = input('Digite um CPF: ')
-    novo_cpf = cpf[:-2]                
-    reverso = 10                        
+    newCPF = cpf[:-2]                
+    reverse = 10                        
     total = 0
 
     for index in range(19):
         if index > 8:                   
             index -= 9                  
 
-        total += int(novo_cpf[index]) * reverso  
+        total += int(newCPF[index]) * reverse  
 
-        reverso -= 1                    
-        if reverso < 2:
-            reverso = 11
+        reverse -= 1                    
+        if reverse < 2:
+            reverse = 11
             d = 11 - (total % 11)
 
             if d > 9:                   
                 d = 0
             total = 0                   
-            novo_cpf += str(d)          
+            newCPF += str(d)          
 
    
-    sequencia = novo_cpf == str(novo_cpf[0]) * len(cpf)
+    sequence = newCPF == str(newCPF[0]) * len(cpf)
 
   
-    if cpf == novo_cpf and not sequencia:
+    if cpf == newCPF and not sequence:
         print('Válido')
     else:
         print('Inválido')
